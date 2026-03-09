493cd6dcd408311d8b310723c9f9b9ed6dcf6fcb
youngjojin <129725415+youngjojin@users.noreply.github.com>
2026-03-09 09:27:19 +0900

    Create test_calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+from calculator import add, subtract, multiply
+
+
+def test_add():
+    assert add(2, 3) == 5
+
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+
+
+def test_multiply():
+    assert multiply(3, 4) == 12
